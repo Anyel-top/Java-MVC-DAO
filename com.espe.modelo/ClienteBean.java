ba84dea74a7ae8258e196702870afe07ea9c1e55
Angel Paul Patiño Diaz <146494692+Anyel-top@users.noreply.github.com>
2024-01-08 07:45:13 -0500

    Add files via upload

---
+++ com.espe.modelo/ClienteBean.java
@@ -0,0 +1,53 @@
+package com.espe.modelo;
+
+public class ClienteBean {
+    private int idCli;
+    private String nombreCli;
+    private String apellidoCli;
+    private String cedulaCli;
+
+    public ClienteBean() {
+    }
+
+    public int getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(int idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getNombreCli() {
+        return nombreCli;
+    }
+
+    public void setNombreCli(String nombreCli) {
+        this.nombreCli = nombreCli;
+    }
+
+    public String getApellidoCli() {
+        return apellidoCli;
+    }
+
+    public void setApellidoCli(String apellidoCli) {
+        this.apellidoCli = apellidoCli;
+    }
+
+    public String getCedulaCli() {
+        return cedulaCli;
+    }
+
+    public void setCedulaCli(String cedulaCli) {
+        this.cedulaCli = cedulaCli;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteBean{" +
+                "idCli=" + idCli +
+                ", nombreCli='" + nombreCli + '\'' +
+                ", apellidoCli='" + apellidoCli + '\'' +
+                ", cedulaCli='" + cedulaCli + '\'' +
+                '}';
+    }
+}
---
+++ com.espe.vista/ClienteVista.java
@@ -0,0 +1,7 @@
+package com.espe.vista;
+
+public class ClienteVista {
+    public void impimirDatosCliente(int id, String nombreCli, String apellidoCli, String cedula){
+        
+    }
+}
